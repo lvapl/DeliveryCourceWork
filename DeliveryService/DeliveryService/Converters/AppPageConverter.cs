bc719f401847541c4a8aeb101f439522eda42c04
lvapl <clashgoglvap@gmail.com>
2023-04-04 12:40:10 +0300

    Сделал конвертер страниц

---
+++ DeliveryService/DeliveryService/Converters/AppPageConverter.cs
@@ -0,0 +1,46 @@
+﻿using DeliveryService.Enums;
+using DeliveryService.MVVM.View;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace DeliveryService.Converters
+{
+    public class AppPageConverter : IValueConverter
+    {
+        public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is AppPages && targetType == typeof(Page))
+            {
+                switch (value) 
+                {
+                    case AppPages.WorkerGeneralInfo:
+                        return new WorkerGeneralInfoPage();
+                    default:
+                        return null;
+                }
+            }
+            return null;
+        }
+
+        public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Page && targetType == typeof(AppPages))
+            {
+                switch(value)
+                {
+                    case WorkerGeneralInfoPage:
+                        return AppPages.WorkerGeneralInfo;
+                    default:
+                        return null;
+                }
+            }
+            return null;
+        }
+    }
+}
--- DeliveryService/DeliveryService/MVVM/ViewModel/MainWindowViewModel.cs
+++ DeliveryService/DeliveryService/MVVM/ViewModel/MainWindowViewModel.cs
@@ -1,14 +1,17 @@
-﻿using DeliveryService.MVVM.View;
+﻿using DeliveryService.Enums;
+using DeliveryService.MVVM.View;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace DeliveryService.MVVM.ViewModel
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public WorkerGeneralInfoPage WorkerGeneralInfoPage { get; set; } = new WorkerGeneralInfoPage();
+        public AppPages CurrentPage { get; set; } = AppPages.WorkerGeneralInfo;
+
     }
 }
