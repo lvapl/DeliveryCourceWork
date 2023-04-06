b376fae229b4e51ca5f66a2477d411664a95bc03
lvapl <clashgoglvap@gmail.com>
2023-04-06 11:17:08 +0300

    Сделал интерфейс главного окна

--- DeliveryService/DeliveryService/MVVM/View/MainWindow.xaml.cs
+++ DeliveryService/DeliveryService/MVVM/View/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -9,6 +10,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
@@ -23,7 +25,17 @@ namespace DeliveryService.MVVM.View
         public MainWindow()
         {
             InitializeComponent();
-            this.DataContext = new MainWindowViewModel();
+            this.DataContext = new MainWindowViewModel(this);
+        }
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessage(IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+        private void spControlPanel_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            WindowInteropHelper helper = new WindowInteropHelper(this);
+
+            SendMessage(helper.Handle, 161, 2, 0);
         }
     }
 }
--- DeliveryService/DeliveryService/MVVM/ViewModel/MainWindowViewModel.cs
+++ DeliveryService/DeliveryService/MVVM/ViewModel/MainWindowViewModel.cs
@@ -5,15 +5,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace DeliveryService.MVVM.ViewModel
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public AppPages CurrentPage { get; set; } = AppPages.WorkerGeneralInfo;
+        private Window _window;
 
         private RelayCommand? _changePageCommand;
+
+        private RelayCommand? _closeWindowCommand;
+
+        private RelayCommand? _maximazeWindowCommand;
+
+        private RelayCommand? _minimizeWindowCommand;
+
+
+        public double MaxHight
+        {
+            get
+            {
+                return SystemParameters.MaximizedPrimaryScreenHeight;
+            }
+        }
+
+        public AppPages CurrentPage { get; set; } = AppPages.WorkerGeneralInfo;
 
         public RelayCommand ChangePageCommand
         {
@@ -22,8 +40,55 @@ namespace DeliveryService.MVVM.ViewModel
                 return _changePageCommand ?? (_changePageCommand = new RelayCommand((object? obj) =>
                 {
                     CurrentPage = (AppPages)obj;
+                    
                 }));
             }
+        }
+
+        public RelayCommand CloseWindowCommand
+        {
+            get
+            {
+                return _closeWindowCommand ?? (_closeWindowCommand = new RelayCommand((object? obj) =>
+                {
+                    _window.Close();
+                }));
+            }
+        }
+
+        public RelayCommand MinimizeWindowCommand
+        {
+            get
+            {
+                return _minimizeWindowCommand ?? (_minimizeWindowCommand = new RelayCommand((object? obj) =>
+                {
+                    _window.WindowState = WindowState.Minimized;
+                }));
+            }
+        }
+
+        public RelayCommand MaximazeWindowCommand
+        {
+            get
+            {
+                return _maximazeWindowCommand ?? (_maximazeWindowCommand = new RelayCommand((object? obj) =>
+                {
+                    if (_window.WindowState == WindowState.Maximized)
+                    {
+                        _window.WindowState = WindowState.Normal;
+                    }
+                    else
+                    {
+                        _window.WindowState = WindowState.Maximized;
+                    }
+                }));
+            }
+        }
+
+
+        public MainWindowViewModel(Window window)
+        {
+            _window = window;
         }
     }
 }
