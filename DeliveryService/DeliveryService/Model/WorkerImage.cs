02ebd38a0bd9b364d33111e3e6af25ed2945f01c
lvapl <clashgoglvap@gmail.com>
2023-04-11 09:12:57 +0300

    Изменена база данных и переработана структура

---
+++ DeliveryService/DeliveryService/Model/WorkerImage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DeliveryService.Model;
+
+public partial class WorkerImage
+{
+    public int Id { get; set; }
+
+    public byte[] WorkerImage1 { get; set; } = null!;
+
+    public virtual ICollection<Worker> Workers { get; } = new List<Worker>();
+}
